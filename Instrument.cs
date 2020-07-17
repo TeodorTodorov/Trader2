b4f4c5abae50e894f9aec3b7f7405fe30497f602
TeodorTodorov <teodorvulchev@gmail.com>
2020-07-17 12:20:02 +0300

    Delete duplicated files

--- Instrument.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-public class Instrument
-{
-    public string Symbol { get; set; }
-    public double TickSize { get; set; }
-    public double Volume24H { get; set; }
-}
--- OrderBook.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-public class OrderBook
-{
-    public string Side { get; set; }
-    public double Price { get; set; }
-    public int Size { get; set; }
-}
